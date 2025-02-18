3bf9c55a48454215b76f297bbfcfb448df4622d6
Henry Rovnyak <h.rovnyak@proton.me>
2025-02-18 17:03:02 -0500

    Fix errors/warnings

--- frontend/src/routes/project/+page.svelte
+++ frontend/src/routes/project/+page.svelte
@@ -1,45 +1,45 @@
 <script>
     import { ProjectState } from "../../lib/project_state.svelte"
 
-    let state = $state(new ProjectState())
+    let project = $state(new ProjectState())
 
-    let key = "Key"
-    let value = "Value"
+    let key = $state("Key")
+    let value = $state("Value")
 </script>
 
 <h1>Project page!</h1>
 
 <input
     value="X"
-    oninput={e => state.updateInProject("reactive_testing.bruh", e.currentTarget.value)}
+    oninput={e => project.updateInProject("reactive_testing.bruh", e.currentTarget.value)}
 />
 
-<p>{state.reactive_testing.bruh}</p>
+<p>{project.reactive_testing.bruh}</p>
 
 <button
     onclick={() => {
-        state.appendInProject("reactive_testing.list", state.reactive_testing.bruh)
+        project.appendInProject("reactive_testing.list", project.reactive_testing.bruh)
     }}>Append</button
 >
 
 <br />
 
-{#each state.reactive_testing.list as value}
+{#each project.reactive_testing.list as value}
     <p>{value}</p>
 {/each}
 
 <button
     onclick={() => {
-        state.deleteInProject(
-            `reactive_testing.list[${Math.floor(state.reactive_testing.list.length / 2)}]`,
+        project.deleteInProject(
+            `reactive_testing.list[${Math.floor(project.reactive_testing.list.length / 2)}]`,
         )
     }}>Delete Halfway</button
 >
 <br />
 <br />
 
-{#each Object.keys(state.reactive_testing.values) as value}
-    <p>{value}: {state.reactive_testing.values[value]}</p>
+{#each Object.keys(project.reactive_testing.values) as value}
+    <p>{value}: {project.reactive_testing.values[value]}</p>
 {/each}
 
 <input bind:value={key} />
@@ -47,17 +47,17 @@
 
 <button
     onclick={() => {
-        if (state.reactive_testing.values[key] == undefined) {
-            state.appendInProject(`reactive_testing.values.${key}`, value)
+        if (project.reactive_testing.values[key] == undefined) {
+            project.appendInProject(`reactive_testing.values.${key}`, value)
         } else {
-            state.updateInProject(`reactive_testing.values.${key}`, value)
+            project.updateInProject(`reactive_testing.values.${key}`, value)
         }
     }}>Insert/Update</button
 ><br />
 <button
     onclick={() => {
-        if (state.reactive_testing.values[key] != undefined) {
-            state.deleteInProject(`reactive_testing.values.${key}`)
+        if (project.reactive_testing.values[key] != undefined) {
+            project.deleteInProject(`reactive_testing.values.${key}`)
         }
     }}>Delete</button
 >
@@ -65,14 +65,14 @@
 <br />
 <br />
 
-<button onclick={() => state.updateInProject("button_state", "enabled")}>Enable selector</button>
+<button onclick={() => project.updateInProject("button_state", "enabled")}>Enable selector</button>
 <input
     type="radio"
     name="options"
     id="A"
     value="A"
-    disabled={state.button_state != "enabled"}
-    oninput={() => state.select("a")}
+    disabled={project.button_state != "enabled"}
+    oninput={() => project.select("a")}
 />
 <label for="A">A</label>
 <input
@@ -80,8 +80,8 @@
     name="options"
     id="B"
     value="B"
-    disabled={state.button_state != "enabled"}
-    oninput={() => state.select("b")}
+    disabled={project.button_state != "enabled"}
+    oninput={() => project.select("b")}
 />
 <label for="B">B</label>
 
