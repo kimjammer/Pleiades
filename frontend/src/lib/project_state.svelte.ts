5ef97c7b3a12350cba45cd33b43fd75ff158ed42
Henry Rovnyak <h.rovnyak@proton.me>
2025-02-18 16:50:35 -0500

    Stub implementations of append and update in project

--- frontend/src/lib/project_state.svelte.ts
+++ frontend/src/lib/project_state.svelte.ts
@@ -1,14 +1,97 @@
+class ReactiveTesting {
+    bruh = $state("X")
+    list = $state(["A", "B", "C"])
+    values: Record<string, string> = $state({
+        x: "x",
+        y: "y",
+        z: "z",
+    })
+}
+
 export class ProjectState {
     button_state: "enabled" | "a" | "b" = $state("enabled")
 
+    reactive_testing = $state(new ReactiveTesting())
+
     select(option: "a" | "b") {
-        console.log(option)
         if (this.button_state == "enabled") {
             this.button_state = option
         }
     }
 
-    enable() {
-        this.button_state = "enabled"
+    appendInProject(key: string, value: any) {
+        traverseObject(
+            this,
+            toPath(key),
+            (object, key) => {
+                if (object[key] == undefined) {
+                    object[key] = value
+                } else if (Array.isArray(object[key])) {
+                    object[key].push(value)
+                } else {
+                    throw `Unsure how to append ${value} to ${key}. Did you mean 'updateInProject'?`
+                }
+            },
+            (array, idx) => {
+                if (Array.isArray(array[idx])) {
+                    array[idx].push(value)
+                } else {
+                    throw `Unsure how to append ${value} to ${key}. Did you mean 'updateInProject'?`
+                }
+            },
+        )
+    }
+
+    updateInProject(key: string, value: any) {
+        traverseObject(
+            this,
+            toPath(key),
+            (object, key) => {
+                object[key] = value
+            },
+            (array, idx) => {
+                array[idx] = value
+            },
+        )
+    }
+
+    deleteInProject(key: string) {
+        console.log(key, toPath(key))
+        traverseObject(
+            this,
+            toPath(key),
+            (object, key) => {
+                delete object[key]
+            },
+            (array, idx) => {
+                array.splice(idx, 1)
+            },
+        )
+    }
+}
+
+function toPath(data: string): string[] {
+    // The `replaceAll` replaces instances of `things[5].etc` with `things.5.etc`
+    return data.replaceAll(/\[([^\]]*)\]/g, (_, idx) => `.${idx}`).split(".")
+}
+
+function traverseObject(
+    object: any,
+    key: string[],
+    toAffectObject: (final_spot: any, final_key: string) => void,
+    toAffectArray: (final_spot: Array<any>, final_key: number) => void,
+) {
+    if (Array.isArray(object)) {
+        if (key.length == 1) {
+            toAffectArray(object, parseInt(key[0]))
+        } else {
+            traverseObject(object[parseInt(key[0])], key.slice(1), toAffectObject, toAffectArray)
+        }
+    } else {
+        if (key.length == 1) {
+            toAffectObject(object, key[0])
+        } else {
+            traverseObject(object[key[0]], key.slice(1), toAffectObject, toAffectArray)
+        }
     }
 }
--- frontend/src/routes/project/+page.svelte
+++ frontend/src/routes/project/+page.svelte
@@ -2,11 +2,70 @@
     import { ProjectState } from "../../lib/project_state.svelte"
 
     let state = $state(new ProjectState())
+
+    let key = "Key"
+    let value = "Value"
 </script>
 
 <h1>Project page!</h1>
 
-<button onclick={() => state.enable()}>Enable selector</button>
+<input
+    value="X"
+    oninput={e => state.updateInProject("reactive_testing.bruh", e.currentTarget.value)}
+/>
+
+<p>{state.reactive_testing.bruh}</p>
+
+<button
+    onclick={() => {
+        state.appendInProject("reactive_testing.list", state.reactive_testing.bruh)
+    }}>Append</button
+>
+
+<br />
+
+{#each state.reactive_testing.list as value}
+    <p>{value}</p>
+{/each}
+
+<button
+    onclick={() => {
+        state.deleteInProject(
+            `reactive_testing.list[${Math.floor(state.reactive_testing.list.length / 2)}]`,
+        )
+    }}>Delete Halfway</button
+>
+<br />
+<br />
+
+{#each Object.keys(state.reactive_testing.values) as value}
+    <p>{value}: {state.reactive_testing.values[value]}</p>
+{/each}
+
+<input bind:value={key} />
+<input bind:value /><br />
+
+<button
+    onclick={() => {
+        if (state.reactive_testing.values[key] == undefined) {
+            state.appendInProject(`reactive_testing.values.${key}`, value)
+        } else {
+            state.updateInProject(`reactive_testing.values.${key}`, value)
+        }
+    }}>Insert/Update</button
+><br />
+<button
+    onclick={() => {
+        if (state.reactive_testing.values[key] != undefined) {
+            state.deleteInProject(`reactive_testing.values.${key}`)
+        }
+    }}>Delete</button
+>
+
+<br />
+<br />
+
+<button onclick={() => state.updateInProject("button_state", "enabled")}>Enable selector</button>
 <input
     type="radio"
     name="options"
@@ -25,3 +84,17 @@
     oninput={() => state.select("b")}
 />
 <label for="B">B</label>
+
+<style>
+    input,
+    button {
+        border: 1px solid black;
+        margin: 4px;
+        padding-left: 4px;
+        padding-right: 4px;
+    }
+
+    button {
+        border-radius: 4px;
+    }
+</style>
