a0f23a82b317a92f7559fd34768ffdbb002e869d
KimJammer <41841812+kimjammer@users.noreply.github.com>
2025-02-18 22:06:35 -0500

    feat(frontend): Install shadcn-svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Button } from "$lib/components/ui/button/index"
+</script>
+
+<div>
+    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Pleiades
+    </h1>
+
+    <h2
+        class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+    >
+        Collaborate Easier...or else...
+    </h2>
+
+    <Button>Create your first Project</Button>
+</div>
