2b26dbe6894a911bbe519ab7c8c6a0a02a8d64aa
KimJammer <41841812+kimjammer@users.noreply.github.com>
2025-02-18 22:41:10 -0500

    feat(db): Connect to DB

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
+	"os"
 )
 
+var mongoClient *mongo.Client
+
 func main() {
+	//Connect to database
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Println("Set your 'MONGODB_URI' environment variable as in .env.example. Skipping database connection.")
+	} else {
+		mongoClient, err := mongo.Connect(options.Client().ApplyURI(uri))
+		if err != nil {
+			panic(err)
+		}
+		var result bson.M
+		if err := mongoClient.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+			panic(err)
+		}
+		log.Println("Successfully connected to MongoDB")
+		defer func() {
+			if err := mongoClient.Disconnect(context.TODO()); err != nil {
+				panic(err)
+			}
+		}()
+	}
+	
+	// Setup webserver
 	router := gin.Default()
 	router.GET("/ws", wsEndpoint)
-
 	router.GET("/projects", projectsHandler)
 
 	router.Run(":8080")
