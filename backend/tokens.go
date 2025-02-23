1c6d6a217b609e5999c0ff13a4d10b4775616c41
Henry Rovnyak <h.rovnyak@proton.me>
2025-02-23 18:40:22 -0500

    Stub implementations of tokens

---
+++ backend/tokens.go
@@ -0,0 +1,11 @@
+package main
+
+// Create a token for the given user ID
+func make_token(userId string) string {
+	return userId
+}
+
+// Validate the token for the given user ID, returns `nil` if the token is invalid.
+func verify_token(token string) string {
+	return token
+}
