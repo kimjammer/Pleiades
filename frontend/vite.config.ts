b381e92b411c325c453176b86780c194e93b392a
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-03-04 23:46:48 +0000

    switch to npm for real

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,4 @@ import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		fs: {
-			// allow: [
-			// 	searchForWorkspaceRoot(process.cwd()),
-			// 	"./packages",
-			// ],
-			strict: false, // TODO: remove this after troubleshooting why forbidden
-		},
-	},
 });
