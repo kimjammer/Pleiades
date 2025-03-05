4d3a5077eabce59acc59c244ea743eb422061c9d
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-03-05 00:52:55 +0000

    add tz picker

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import ManualInput from "$lib/availability/ManualInput.svelte";
     import {weekdayDateRanges} from "$lib/availability/timeutils";
+    import TzPicker from "$lib/components/ui/TzPicker.svelte"
   
     let tzOffset = 0;
     const ranges = weekdayDateRanges();
-  </script>
+</script>
   
-  <ManualInput
-      {ranges}
-      {tzOffset}
-      shouldUseWeekdays={true}
-  />
+<TzPicker />
+
+<ManualInput
+    {ranges}
+    {tzOffset}
+    shouldUseWeekdays={true}
+/>
   
