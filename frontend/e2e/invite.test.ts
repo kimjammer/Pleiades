44e18de9095a2074a68e8215e35245b7fe4af246
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-03-07 22:40:13 +0000

    rm unused test

--- frontend/e2e/invite.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test"
-
-test("Create Project and open Project Page", async ({ page }) => {
-    await page.goto("/home")
-    await page.getByRole("link", { name: "Title Description" }).click()
-    await new Promise(r => setTimeout(r, 1000))
-    await expect(page.locator("h2")).toContainText("Title")
-    await expect(page.getByRole("paragraph")).toContainText("Description")
-})
