bc50cb08c8ec0d6041c0876d584887887f09b69d
Catherine Harrison <cate.e.harrison@gmail.com>
2025-03-26 12:21:42 -0400

    small work on polls

--- frontend/src/routes/project/polls/PollsTab.svelte
+++ frontend/src/routes/project/polls/PollsTab.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
     import * as Tabs from "$lib/components/ui/tabs"
     import type { ProjectState } from "$lib/project_state.svelte.ts"
+    import { Button } from "$lib/components/ui/button/index"
+    import CreationModal from "../tasks/CreationModal.svelte";
+    import DueDate from "$lib/components/DueDate.svelte";
+    import TimeEstimate from "$lib/components/TimeEstimate.svelte";
 
     let { project }: { project: ProjectState } = $props()
+
+    async function createPoll() {
+        //TODO: send info to create poll
+    }
+
 </script>
 
 <Tabs.Content value="polls">
     <div>Todo: Team polls here!</div>
+    <Button onclick={createPoll}>Create New Poll</Button>
 </Tabs.Content>
