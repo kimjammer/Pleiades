2791a1b61c5a23d0e1629dc19b4873d6d386a7f5
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-04-05 14:42:17 +0000

    Document this week (0.14hr)  (0.01hr)

--- .time/get_log.py
+++ .time/get_log.py
@@ -5,16 +5,19 @@ def process_git_log():
     cmd = ['git', 'log', '--author=' + '"' + subprocess.getoutput('git config user.name') + '"',
            '--pretty=format:"%ad %h %s"', '--date=short']
     print(' '.join(cmd))
+    total_time = 0
     output = subprocess.getoutput(' '.join(cmd))
-    for line in output.split("\n"):
+    for line in output.split("\n")[::-1]:
         extracted = re.findall(r'\(([^)]+)\)', line)
         cleaned = re.sub(r'\s*\([^)]+\)', '', line)
         try:
             time = extracted[0]
-        except IndexError:
+            total_time += float(time.replace("hr", ""))
+        except (IndexError, ValueError):
             time = "\t"
         [date, git_hash, *msg] = cleaned.split(" ")
         print(date, git_hash, time, " ".join(msg))
+    print("\ntotal time:", round(total_time, 2))
 
 if __name__ == "__main__":
     process_git_log()
