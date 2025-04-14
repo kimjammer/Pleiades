e57f9991034af2b6ae5e00c120c05baae9893356
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2025-04-14 16:33:28 +0000

    Play with signin (0.36hr)  (0.01hr) (0.02hr)

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,6 +2,7 @@
     import { goto } from "$app/navigation"
     import { base } from "$app/paths"
     import { PUBLIC_API_HOST, PUBLIC_PROTOCOL } from "$env/static/public"
+    import GoogleSignin from "$lib/components/GoogleSignin.svelte"
     import { Button } from "$lib/components/ui/button/index"
     import { Input } from "$lib/components/ui/input/index"
     import { recordEvent, tryJoinProject } from "$lib/restApi"
@@ -36,10 +37,10 @@
 </script>
 
 <div class="grid grid-cols-1 grid-rows-3 sm:h-dvh sm:grid-cols-3 sm:grid-rows-1">
-    <div class="row-span-1 bg-primary p-10 dark:bg-secondary sm:col-span-1">
+    <div class="bg-primary dark:bg-secondary row-span-1 p-10 sm:col-span-1">
         <a href={base + "/"}>
             <h1
-                class="scroll-m-20 text-4xl font-extrabold tracking-tight text-primary-foreground dark:text-secondary-foreground lg:text-5xl"
+                class="text-primary-foreground dark:text-secondary-foreground scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
             >
                 Pleiades
             </h1>
@@ -68,6 +69,7 @@
                 bind:value={password}
             />
             <Button onclick={login}>Login</Button>
+            <GoogleSignin />
             <Button
                 variant="link"
                 href="forgotPassword">Forgot Password?</Button
