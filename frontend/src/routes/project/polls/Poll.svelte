692637cff68672f1b2b23247549a1b2261d41891
Henry Rovnyak <h.rovnyak@proton.me>
2025-04-14 12:32:45 -0400

    Documents and basic poll UI

---
+++ frontend/src/routes/project/polls/Poll.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import type { ProjectState, Poll } from "$lib/project_state.svelte"
+    import { Button } from "$lib/components/ui/button"
+
+    let { project, poll }: { project: ProjectState; poll: Poll } = $props()
+</script>
+
+<div class="poll mt-[0.5em] border">
+    <h1 class="mb-[0.3em] text-xl">{poll.title}</h1>
+    {#if poll.description != ""}
+        <p class="mb-[0.5em] opacity-80">{poll.description}</p>
+    {/if}
+
+    <div class="options-grid">
+        {#each poll.options as option}
+            <p class="option-name">{option.title}</p>
+            <Button
+                class="hover:border-transparent hover:bg-green-400/40 dark:hover:bg-green-700/40"
+                variant="outline"
+                size="icon">✅</Button
+            >
+            <Button
+                class="hover:border-transparent hover:bg-yellow-400/40 dark:hover:bg-yellow-700/40"
+                variant="outline"
+                size="icon">🟡</Button
+            >
+            <Button
+                class="hover:border-transparent hover:bg-red-400/40 dark:hover:bg-red-700/40"
+                variant="outline"
+                size="icon">❌</Button
+            >
+        {/each}
+    </div>
+</div>
+
+<style>
+    .poll {
+        padding: 0.5em;
+        border-radius: 0.5em;
+        width: fit-content;
+    }
+
+    .options-grid {
+        display: grid;
+        grid-template-columns: 1fr min-content min-content min-content;
+        column-gap: 0.2em;
+        row-gap: 0.4em;
+    }
+
+    .option-name {
+        align-self: center;
+        margin-right: 0.5em;
+        text-align: right;
+    }
+</style>
--- frontend/src/routes/project/polls/PollsTab.svelte
+++ frontend/src/routes/project/polls/PollsTab.svelte
@@ -1,27 +1,14 @@
 <script lang="ts">
     import * as Tabs from "$lib/components/ui/tabs"
-    import type { ProjectState } from "$lib/project_state.svelte.ts"
+    import type { ProjectState } from "$lib/project_state.svelte"
     import type { PageData } from "./$types"
     import CreationModal from "./CreationModal.svelte"
-    import { onMount } from "svelte"
-    import { PUBLIC_API_HOST, PUBLIC_PROTOCOL } from "$env/static/public"
-    import { goto } from "$app/navigation"
-    import { base } from "$app/paths"
-    import { toast } from "svelte-sonner"
-    import type { PollsResponse } from "$lib/schema.js"
-    import { Button } from "$lib/components/ui/button"
-    import PollDueDate from "./PollDueDate.svelte"
-    import {Poll} from "$lib/project_state.svelte.ts";
+    import PollComponent from "./Poll.svelte"
 
     let { project, data }: { project: ProjectState; data: PageData } = $props()
     console.log("data in PollsTab: " + data)
 
-    let showForm = false
     let polls = project.polls //string of poll titles
-
-    function handlePollClick(poll: Poll) {
-        console.log("Poll clicked:", poll.title, " ", poll.dueDate)
-    }
 </script>
 
 <Tabs.Content value="polls">
@@ -33,14 +20,10 @@
     <div>
         {#if polls.length > 0}
             {#each polls as poll}
-                <div>
-                    <Button onclick={() => handlePollClick(poll)}>
-                        {poll.title}
-                        <div>
-                            Due: {poll.dueDate}
-                        </div>
-                    </Button>
-                </div>
+                <PollComponent
+                    {project}
+                    {poll}
+                />
             {/each}
         {:else}
             <p>No polls available.</p>
